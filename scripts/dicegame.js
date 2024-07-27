4feea814108f2cc46bf0e5857f4437134bba906b
Rauzzy <mguerra10@my.bcit.ca>
2024-07-27 00:12:38 -0700

    V1.09
    Pair of dice is now displayed on the players dice container when roll dice is clicked.

--- scripts/dicegame.js
+++ scripts/dicegame.js
@@ -26,8 +26,57 @@ class dice{
     }
 
     describeSelf(){
-        let returnImage = `<img src="images/dice-${this.#face}.jpg" alt="dice-${this.#face}">`
+        let returnImage = `<img class="dice-img" src="images/dice-${this.#face}.jpg" alt="dice-${this.#face}">`
         return returnImage
+
+    
     }
 
 }
+//@author Marius Ruzzel Guerra
+class Player
+{   #playerName;
+    #dice;
+    constructor(playerName)
+    {
+        this.#playerName = playerName;
+        this.#dice = new dice(1,1);
+    }
+
+    rollDice()
+    {
+        const face = Math.floor(Math.random()*6) +1;
+        this.#dice.setFace(face);
+        this.#dice.setValue(face);
+        return new dice(face, face);
+    }
+}
+
+function rollDiceAndDisplay()
+{
+    const player1 = new Player("Player1");
+    const player2 = new Player("Player2");
+
+    //player dice pair
+    const rolledDice1 = player1.rollDice();
+    const rolledDice2 = player1.rollDice()
+
+    //computer dice pair
+    const rolledDice3 = player2.rollDice()
+    const rolledDice4 = player2.rollDice()
+
+
+    const diceContainer1 = document.getElementById("dice-container1");
+    const diceContainer2 = document.getElementById("dice-container2");
+
+    diceContainer1.innerHTML = "";
+    diceContainer1.innerHTML = rolledDice1.describeSelf();
+    diceContainer1.innerHTML += rolledDice2.describeSelf();
+
+
+    diceContainer2.innerHTML = "";
+    diceContainer2.innerHTML = rolledDice3.describeSelf();
+    diceContainer2.innerHTML += rolledDice4.describeSelf();
+}
+
+document.getElementById("roll").addEventListener("click", rollDiceAndDisplay);
